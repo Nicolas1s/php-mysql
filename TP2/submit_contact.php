6ad9f08a5b37737a44df8017c2a2e242069aad0f
Nicolas1s <115209939+Nicolas1s@users.noreply.github.com>
2023-10-05 00:10:27 +0200

    Testez la présence d'un paramètre

--- TP2/submit_contact.php
+++ TP2/submit_contact.php
@@ -23,23 +23,15 @@
         ?>
 
         <!-- inclusion de l'entête du site -->
-        
-        <article>
+        <?php include_once('header.php'); ?>
 
-            <h1>Message bien reçu !</h1>
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Rappel de vos informations</h5>
-                    <p class="card-text"><b>Email</b> :
-                        <?php echo $_GET['email']; ?>
-                    </p>
-                    <p class="card-text"><b>Message</b> :
-                        <?php echo $_GET['message']; ?>
-                    </p>
-                </div>
-            </div>
-
-        </article>
+        <?php
+        if (isset($_GET['email']) && isset($_GET['message'])) {
+        } 
+        else {
+            echo "<h1>Il faut un email et un message pour soumettre le formulaire.</h1>";
+        }
+        ?>
 
     </div>
 
