cc868ee0ca61a5f93878308007f0f5275743c37b
Nicolas1s <115209939+Nicolas1s@users.noreply.github.com>
2023-10-05 00:24:23 +0200

    Privilégiez la méthode post

--- TP2/submit_contact.php
+++ TP2/submit_contact.php
@@ -26,13 +26,14 @@
         <?php include_once('header.php'); ?>
 
         <?php
-        if (
-            (!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL))
-            || (!isset($_GET['message']) || empty($_GET['message']))
-        ) {
-            echo ('Il faut un email et un message valides pour soumettre le formulaire.');
-            return;
-        }
+            if (
+                (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+                || (!isset($_POST['message']) || empty($_POST['message']))
+                )
+            {
+                echo('<h1>Il faut un email et un message valides pour soumettre le formulaire.</h1>');
+                return;
+            }
         ?>
 
     </div>
