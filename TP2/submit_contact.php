8e54c93df9967bc0efb5ce48e58b823caf4ae0b5
Nicolas1s <115209939+Nicolas1s@users.noreply.github.com>
2023-10-05 00:51:52 +0200

    Traitez l'envoi en PHP

--- TP2/submit_contact.php
+++ TP2/submit_contact.php
@@ -24,24 +24,48 @@
 
         <!-- inclusion de l'entête du site -->
         <?php include_once('header.php'); ?>
-        
+
         <?php
-        
-            if (
-                (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
-                || (!isset($_POST['message']) || empty($_POST['message']))
-                )
-            {
-                echo('<h1>Il faut un email et un message valides pour soumettre le formulaire.</h1>');
-                return;
-            }
+
+        if (
+            (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+            || (!isset($_POST['message']) || empty($_POST['message']))
+        ) {
+            echo ('<h1>Il faut un email et un message valides pour soumettre le formulaire.</h1>');
+            return;
+        }
         ?>
-        
+
         <strong>Badaboum</strong>
         <p><b>Message</b> : <strong>Badaboum</strong></p>
-        <p><b>Message</b> : <script>alert('Badaboum')</script></p>
-        <p><b>Message</b> : <?php echo htmlspecialchars($_POST['message']); ?> </p>
+        <p><b>Message</b> :
+            <script>alert('Badaboum')</script>
+        </p>
+        <p><b>Message</b> :
+            <?php echo htmlspecialchars($_POST['message']); ?>
+        </p>
         <p><b>Message</b> : &lt;strong&gt;Badaboum&lt;/strong&gt; ?></p>
+
+        <?php
+        // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+        if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0) {
+            // Testons si le fichier n'est pas trop gros
+            if ($_FILES['screenshot']['size'] <= 1000000) {
+                // Testons si l'extension est autorisée
+                $fileInfo = pathinfo($_FILES['screenshot']['name']);
+                $extension = $fileInfo['extension'];
+                $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+                if (in_array($extension, $allowedExtensions)) {
+                    // On peut valider le fichier et le stocker définitivement
+                    move_uploaded_file(
+                        $_FILES['screenshot']['tmp_name'],
+                        'uploads/' . basename($_FILES['screenshot']['name'])
+                    );
+                    echo "L'envoi a bien été effectué !";
+                }
+            }
+        }
+        ?>
 
     </div>
 
