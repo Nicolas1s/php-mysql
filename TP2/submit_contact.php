cdbbf0dde73d3a3ee0eb8a159d2e3f5466119ed7
Nicolas1s <115209939+Nicolas1s@users.noreply.github.com>
2023-10-05 00:16:41 +0200

    Contrôlez la valeur des paramètres

--- TP2/submit_contact.php
+++ TP2/submit_contact.php
@@ -26,10 +26,12 @@
         <?php include_once('header.php'); ?>
 
         <?php
-        if (isset($_GET['email']) && isset($_GET['message'])) {
-        } 
-        else {
-            echo "<h1>Il faut un email et un message pour soumettre le formulaire.</h1>";
+        if (
+            (!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL))
+            || (!isset($_GET['message']) || empty($_GET['message']))
+        ) {
+            echo ('Il faut un email et un message valides pour soumettre le formulaire.');
+            return;
         }
         ?>
 
